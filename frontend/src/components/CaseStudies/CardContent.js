0d3585255bd048ed3fa1e77c71f1418ee67bb03d
Takalani <takimadau@gmail.com>
2023-10-17 16:18:10 +0200

    fix card slider

--- frontend/src/components/CaseStudies/CardContent.js
+++ frontend/src/components/CaseStudies/CardContent.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import '../TestCm/testcm.scss';
 
 const CardContent = ({imageURL, title, paragraph}) => {
-  // const cardStyle = {
-  //   background: `url(${imageURL})`, // Set the background image
-  //   backgroundSize: 'cover',        // Adjust background size as needed
-  // };
-
   return (
-    <div className='card'>
-      <div>
-        <div>
-          <img src={imageURL} alt={title} />
-        </div>
-        <div>
+    <div className='container'>
+      <div className='card'>
+        <img src={imageURL} alt={title} />
+        <div className='content'>
           <div className='line2'></div>
           <h3>{title}</h3>
           <p>{paragraph}</p>
