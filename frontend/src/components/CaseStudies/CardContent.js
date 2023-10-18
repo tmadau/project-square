b65694ff9f80ed9f85d41f32c733d202ed233530
Takalani <takimadau@gmail.com>
2023-10-18 10:33:12 +0200

    remove commented styles

--- frontend/src/components/CaseStudies/CardContent.js
+++ frontend/src/components/CaseStudies/CardContent.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const CardContent = ({imageURL, title, paragraph}) => {
+const CardContent = ({ imageURL, title, paragraph }) => {
+  const customImageWidth = "100%"; // Define your custom image width here
+
+  const imageStyle = {
+    width: customImageWidth,
+  };
+
   return (
-    <div className='container'>
-      <div className='card'>
-        <img src={imageURL} alt={title} />
-        <div className='content'>
-          <div className='line2'></div>
+    <div className="container">
+      <div className="card">
+        <div>
+          <img src={imageURL} al={title} style={imageStyle} />
+        </div>
+        <div className="content">
+          <div className="line2"></div>
           <h3>{title}</h3>
           <p>{paragraph}</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CardContent;
--- frontend/src/components/CaseStudies/CaseStudies.js
+++ frontend/src/components/CaseStudies/CaseStudies.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import Header from '../Header/Header';
-import './case-studies.scss';
-import CardContent from './CardContent';
-import theOlympian from './the-olympian.png';
-import savingsJar from './savings-jar.png';
-import skhoSkho from './skho-skho.png';
-import Slider from 'react-slick';
+import React from "react";
+import Header from "../Header/Header";
+import "./case-studies.scss";
+import CardContent from "./CardContent";
+import theOlympian from "./the-olympian.png";
+import savingsJar from "./savings-jar.png";
+import skhoSkho from "./skho-skho.png";
+import Slider from "react-slick";
 
 const settings = {
   dots: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
-  slidesToScroll: 1
-}
+  slidesToScroll: 1,
+};
 
 // Define responsive settings for different screen sizes
 const responsiveSettings = [
   {
-    breakpoint: 1024, // Example breakpoint
+    breakpoint: 1225, // Example breakpoint
     settings: {
       slidesToShow: 2,
       slidesToScroll: 1,
     },
   },
   {
-    breakpoint: 576, // Another breakpoint
+    breakpoint: 820, // Another breakpoint
     settings: {
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -36,52 +36,53 @@ const responsiveSettings = [
 const cardData = [
   {
     imageURL: theOlympian,
-    title: 'The Olympian',
-    paragraph: 'The only athlete in the world to do her Olympic routine in 2020.'
+    title: "The Olympian",
+    paragraph:
+      "The only athlete in the world to do her Olympic routine in 2020.",
   },
   {
     imageURL: savingsJar,
-    title: 'The Savings Jar',
-    paragraph: 'Grow your savings treasure and grow your dragon.'
+    title: "The Savings Jar",
+    paragraph: "Grow your savings treasure and grow your dragon.",
   },
   {
     imageURL: skhoSkho,
-    title: 'Skhokho seMali',
-    paragraph: 'Helping South Africans become #CashCleva with Skhokho and TymeBank.'
+    title: "Skhokho seMali",
+    paragraph:
+      "Helping South Africans become #CashCleva with Skhokho and TymeBank.",
   },
   {
     imageURL: theOlympian,
-    title: 'The Olympian',
-    paragraph: 'The only athlete in the world to do her Olympic routine in 2020.'
+    title: "The Olympian",
+    paragraph:
+      "The only athlete in the world to do her Olympic routine in 2020.",
   },
   {
     imageURL: savingsJar,
-    title: 'The Savings Jar',
-    paragraph: 'Grow your savings treasure and grow your dragon.'
+    title: "The Savings Jar",
+    paragraph: "Grow your savings treasure and grow your dragon.",
   },
   {
     imageURL: skhoSkho,
-    title: 'Skhokho seMali',
-    paragraph: 'Helping South Africans become #CashCleva with Skhokho and TymeBank.'
-  }
+    title: "Skhokho seMali",
+    paragraph:
+      "Helping South Africans become #CashCleva with Skhokho and TymeBank.",
+  },
 ];
 
 const CaseStudies = () => {
   return (
     <section>
-      <Header 
-        paragraph="Case Studies"
-        heading=""
-      />
-      <div className='card-carousel'>
+      <Header paragraph="Case Studies" heading="" />
+      <div className="card-carousel">
         <Slider {...settings} responsive={responsiveSettings}>
           {cardData.map((card, index) => (
-            <CardContent key={index} {...card} /> 
+            <CardContent key={index} {...card} />
           ))}
         </Slider>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default CaseStudies;
